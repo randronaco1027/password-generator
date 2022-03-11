60cdade4142d753ac14f5e9117d6c5f7bf6adceb
randronaco <randronaco1027@gmail.com>
2022-03-10 23:52:41 -0500

    setup button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
 // Assignment code here
 //Determine password length
-var passwordLength = {
-    passwordLength: window.prompt("How long do you want your password to be? 8-128 Characters"),
-};
+// var passwordLength = {
+//     passwordLength: window.prompt("How long do you want your password to be? 8-128 Characters"),
+// };
 
-for (var i = 0; i < passwordLength; i ++) {
-    //Cycle through this until you get password length
-}
+// for (var i = 0; i < passwordLength; i ++) {
+//     //Cycle through this until you get password length
+// }
 
 //Prompts are stored as text, use this to convert to number
-parseInt(passwordLength);
+// parseInt(passwordLength);
 
 //Reject wrong answer
 var test = function() {
@@ -25,22 +25,23 @@ var test = function() {
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// generateBtn.addEventListener("click", writePassword);
 
 function getPasswordLength() {
-    var userAdjective = prompt("How long would you like your password to be?");
-    alert (userAdjective);
+    let password = prompt("Please enter desired password length");
+    while (password < 8 || password > 128){
+        password = prompt("Password must be between 8 and 128 characters");
+    } 
+    console.log(password)
 }
 //add function ro repeat if < 8 or > 128
-switch () {
-
-}
