dfdca09877f655cc7ddfa2af8482949acd26f4cb
randronaco <randronaco1027@gmail.com>
2022-03-15 01:36:23 -0400

    added commentary

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,42 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-
-var randomNumber = function (min, max) {
-    var value = Math.floor(Math.random() * (max - min) + min);
-    return value;
-};
-
-//Create bucket of all possible variables based off prompts
 var charPool = "";
-var password2 = "";
+var passwordTest = "";
 function generatePassword() {
     var passwordLength = parseInt(prompt("Please enter desired password length"));
-    while (passwordLength < 8 || passwordLength > 128) {
+    //Establish how long password will be (min 8 characters, max 128 characters)
+    while (passwordLength < 8 || passwordLength > 128) { //Needs to ignore non numbers
         passwordLength = parseInt(prompt("Password must be between 8 and 128 characters"));
     };
     var passwordCap = window.prompt("Would you like capital letters?");
+    //Include capital letters as possibility for password
     if (passwordCap === "yes" || passwordCap === "y") {
-        // Increase pool size
         charPool += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
+    };
+    //Include lowercase letters as possibility for password
     var passwordLow = window.prompt("Would you like lower case letters?");
     if (passwordLow === "yes" || passwordLow === "y") {
-        // Increase pool size
         charPool += "abcdefghijklmnopqrstuvwyz";
-    }
+    };
+    //Include special characters as possibility for password
     var passwordSpecial = window.prompt("Would you like special characters?");
     if (passwordSpecial === "yes" || passwordSpecial === "y") {
-        // Increase pool size
         charPool += " !”#$%&()*+,-./:;<=>?@[\]^_`{|}~"
-    }
+    };
+    //Include numbers as possibility for password
     var passwordNum = window.prompt("Would you like numbers?");
     if (passwordNum === "yes" || passwordNum === "y") {
-        //Increase pool size
         charPool += "0123456789";
-    }
-    for (var i = 0; i < passwordLength; i++) {
-        //loop to keep creating characters
-        password2 += randomNumber(0, 9);
     };
-    console.log(charPool);
-}
-
-console.log(password2);
+    //Create password given parameters from above
+    for (var i = 0; i < passwordLength; i++) {
+        passwordTest += charPool.charAt(Math.floor(Math.random() * charPool.length)); //Source at bottom
+    };
+    console.log("Pool of characters is " + charPool);
+    console.log("Password length is " + passwordLength);
+    console.log("Password is " + passwordTest);
+};
 
 //Write password to the #password input
 function writePassword() {
@@ -51,7 +44,8 @@ function writePassword() {
     var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
-    
 }
 
-generateBtn.addEventListener("click", generatePassword());
+generateBtn.addEventListener("click", writePassword());
+
+// Source for passwordTest line at line 34: https://www.codegrepper.com/code-examples/javascript/select+random+character+from+string+javascript
